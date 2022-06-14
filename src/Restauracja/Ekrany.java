34f81ca1d62d78e8f42c17e19911e5eb09c17069
QLAPTOP\kotru <kotruper@gmail.com>
2022-06-14 21:15:28 +0200

    ja pierdole

--- src/Restauracja/Ekrany.java
+++ src/Restauracja/Ekrany.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Restauracja;
 
 interface Ekrany{
@@ -38,43 +37,5 @@ interface Ekrany{
      * @return Zwraca zawartosc ekranu w formie stringa
      */
     String wypiszZawartosc();
-=======
-package Restauracja;
 
-interface Ekrany{
-    /**
-     * Typy ekranow
-     */
-    enum typEkranu {
-        Kuchnia,
-        Grill,
-        Kurczak,
-        Service
-    }
-
-    void przycisk1();
-    void przycisk2();
-    void przycisk3();
-    /**
-     * Zwraca informacje o typie ekranu
-     * @return typEkranu
-     */
-    typEkranu typEkranu();
-
-    /**
-     * Zwraca informacje czy ekran jest aktynwy
-     * @return czyAktywny
-     */
-    boolean czyAktywny();
-    /**
-     * Dodaje nowe zamowienie do ekranu
-     * @param _zamowienie Obiekt zamowienie
-     */
-    void dodajZamowienie(Zamowienie _zamowienie);
-    /**
-     * Wyswietla aktualna zawartosc ekranu
-     * @return Zwraca zawartosc ekranu w formie stringa
-     */
-    String wypiszZawartosc();
->>>>>>> refs/remotes/origin/master
 }
